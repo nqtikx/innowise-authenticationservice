de7c1258c1e0176efdad70bc03184200627b3b18
Nikita <miracleqxz@gmail.com>
2026-02-03 17:49:49 +0300

    initializing project

---
+++ src/test/java/com/innowise/authservice/TestAuthserviceApplication.java
@@ -0,0 +1,12 @@
+package com.innowise.authservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(AuthserviceApplication::main).with(TestcontainersConfiguration.class)
+        .run(args);
+  }
+
+}
